77661c7d5b2df1526ac245659b1e89bc3c38bde0
syedtazyeen <themesyed@gmail.com>
2025-02-02 18:04:24 +0530

    refactor(folder): onboarding

--- src/app/onboarding/_components/select-details.tsx
+++ src/app/onboarding/_components/select-details.tsx
@@ -3,7 +3,6 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Textarea } from "@/components/ui/textarea";
 import { Button } from "@/components/ui/button";
-import { FolderPlus, Pickaxe } from "lucide-react";
 import useOnboardStore from "@/store/onboard";
 import SelectHeader from "./select-header";
 
